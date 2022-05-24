b6e25df1e236fcfab367b1def29b2599b39640b6
ZeeID@MSI <zeeideveloper19@gmail.com>
2022-05-25 01:25:38 +0700

    DB vendor

--- vendor_management.sql
+++ vendor_management.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2022 at 09:15 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.25
+-- Generation Time: May 24, 2022 at 08:20 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,15 +34,45 @@ CREATE TABLE `user` (
   `sandi` varchar(55) NOT NULL,
   `wa` varchar(55) NOT NULL,
   `kode_pasangan` varchar(55) NOT NULL,
-  `role` int(6) NOT NULL DEFAULT 1
+  `role` int(6) NOT NULL DEFAULT 1,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `is_active` int(1) NOT NULL DEFAULT 0,
+  `nama_user` varchar(55) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `email`, `sandi`, `wa`, `kode_pasangan`, `role`) VALUES
-(1, 'jundi', 'jundi', 'jundi', '', 'MASTER911', 911);
+INSERT INTO `user` (`id`, `username`, `email`, `sandi`, `wa`, `kode_pasangan`, `role`, `created_at`, `updated_at`, `deleted_at`, `is_active`, `nama_user`) VALUES
+(1, 'jundi', 'jundi', 'jundi', '', 'MASTER911', 911, NULL, NULL, NULL, 1, 'JUNDI HUSNI ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendor`
+--
+
+CREATE TABLE `vendor` (
+  `id` int(11) NOT NULL,
+  `kode_vendor` varchar(55) NOT NULL,
+  `nama_vendor` varchar(55) NOT NULL,
+  `harga_vendor` int(9) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime NOT NULL,
+  `kode_pasangan` varchar(55) NOT NULL,
+  `jenis_vendor` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `vendor`
+--
+
+INSERT INTO `vendor` (`id`, `kode_vendor`, `nama_vendor`, `harga_vendor`, `created_at`, `updated_at`, `deleted_at`, `kode_pasangan`, `jenis_vendor`) VALUES
+(10, '', 'Fahmi', 2000, '2022-05-25 00:56:27', '2022-05-25 01:15:23', '0000-00-00 00:00:00', 'MASTER911', 'Gedung');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +85,12 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `vendor`
+--
+ALTER TABLE `vendor`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +99,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `vendor`
+--
+ALTER TABLE `vendor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
